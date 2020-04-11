8edbbf94738765b21072e2b20b8d8f0899bcfb98
sowisz <sowa.gregory@gmail.com>
2020-04-11 23:44:22 +0200

    classes split

--- game_joy_service/src/arcadejoy.cpp
+++ game_joy_service/src/arcadejoy.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
-#include <vector>
-#include <fcntl.h>
 #include <pwd.h>
 #include <grp.h>
-#include <dirent.h>
 #include <errno.h>
-#include <string.h>
 #include <unistd.h>
 #include <linux/input.h>
+#include "inputdevice.h"
 
 using namespace std;
 
@@ -53,65 +50,7 @@ bool has_user_group(const string &group_name) {
     return false;
 }
 
-struct InputDeviceList {
-    string device_path;
-    string device_name;
-};
-
-class InputDevice {
-  private:
-    vector<InputDeviceList> devices_;
-    static void FreeRes(int num, dirent **name_list);
-  public:
-    vector<InputDeviceList> ScanDevices(void);
-    void PrintDevices();
-    string GetDeviceByName(const string &name);
-};
-
-
-#define INPUT_DEV_PATH "/dev/input"
-
-void InputDevice::FreeRes( int num, dirent **name_list ) {
-    for(int i=0; i<num; i++) free(name_list[i]);
-    free(name_list);
-}
-
-vector<InputDeviceList> InputDevice::ScanDevices(void) {
-    dirent **name_list;
-
-    int num = scandir(INPUT_DEV_PATH, &name_list, [](auto ent)-> int{return ent->d_type==DT_CHR;}, alphasort);
-
-    devices_.clear();
-    for( int i=0; i<num; i++) {
-        char name[256];
-        auto path = string(INPUT_DEV_PATH)+"/"+name_list[i]->d_name;
-        auto fd = open(path.c_str(), O_RDONLY);
-
-        if(fd<0) {
-            FreeRes(num, name_list);
-            throw "cannot open device:" +path;
-        }
-        ioctl(fd, EVIOCGNAME(sizeof(name)), name);
-        close(fd);
-        devices_.push_back(InputDeviceList{path, name});
-    }
-    FreeRes(num, name_list);
-    return devices_;
-}
-
-void InputDevice::PrintDevices() {
-    for( auto &dev : devices_)
-        cout << dev.device_path << " " << dev.device_name << endl;
-}
-
-string InputDevice::GetDeviceByName(const string &name) {
-    for( auto &dev : devices_)
-        if( dev.device_name.compare(name)==0) return dev.device_path;
-    throw "get_device_by_name:\"" + name + "\" not found";
-}
-
 #define INPUT_GROUP "input"
-
 
 void joy() {
     if(!has_user_group(INPUT_GROUP)) {
